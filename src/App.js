89d8acc616401ddde85b7cdc1720e191b395bd8c
Jonathan Carnos <jonathan.carnos@gmail.com>
2023-05-20 16:18:03 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React, { createElement, useMemo, useState } from "react";
+
+function LittleComponent({ inputName, bg }) {
+  return (
+    <div className="input" style={{ backgroundColor: bg }}>
+      <label htmlFor={inputName}>{inputName}</label>
+      <input id={inputName} />
+    </div>
+  );
+}
+
+const inputs = [
+  {
+    inputName: "Firstname",
+    bg: "#F00",
+  },
+  {
+    inputName: "Lastname",
+    bg: "#0F0",
+  },
+  {
+    inputName: "Fullname",
+    bg: "#00F",
+  },
+];
 
 function App() {
+  const [index, setIndex] = useState(0);
+
+  console.log(index);
+
+  const memoComponent = useMemo(() => {
+    return createElement(LittleComponent, {
+      inputName: inputs[index].inputName,
+      bg: inputs[index].bg,
+    });
+  }, [index]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button
+        disabled={index + 1 >= inputs.length}
+        onClick={() => {
+          setIndex((state) => state + 1);
+        }}
+      >
+        Next
+      </button>
+      {memoComponent}
     </div>
   );
 }
